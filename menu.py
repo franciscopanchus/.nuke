7b69af7466965f4d08f905c2e8f2109364fc8a46
franciscopanchus <64598246+franciscopanchus@users.noreply.github.com>
2020-05-01 12:44:28 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,24 @@
+#---------------------------------------------------------	
+#menu.py
+#Version 1.0.0
+#Last Update 30/04/20
+#---------------------------------------------------------
+
+import nuke
+import platform 
+
+Win_dir = 'C:\Users\Franc\.nuke'
+MacOSX = ''
+Linux_Dir = '/home/franc/.nuke'
+
+#Set Global directory
+if platform.system() == "Windows":
+	dir = Win_dir
+elif platform.system() == "Darwin":
+	dir = MacOSX_dir
+elif platform.system() == "Linux":
+	dir = Linux_Dir
+else:
+	dir = None
+
 
