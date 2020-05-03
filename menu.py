e4ad85a3d85db79619c9f64f2ec4cbf6483d315c
franciscopanchus <64598246+franciscopanchus@users.noreply.github.com>
2020-05-02 21:45:06 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,11 +1,27 @@
 #---------------------------------------------------------	
 #menu.py
-#Version 1.0.0
-#Last Update 30/04/20
+#Version 1.0.1
+#Last Update 02/05/20
 #---------------------------------------------------------
+
+
+#---------------------------------------------------------
+#GLOBAL PATHS:::::::::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------
+
+nuke.pluginAddPath('./icons')
+nuke.pluginAddPath('./gizmos/bm_NukeTools')
+
+
+#---------------------------------------------------------
+#GLOBAL IMPORTS:::::::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------
+
+
 
 import nuke
 import platform 
+import nukescripts
 
 Win_dir = 'C:\Users\Franc\.nuke'
 MacOSX = ''
@@ -21,4 +37,48 @@ elif platform.system() == "Linux":
 else:
 	dir = None
 
+
+#---------------------------------------------------------
+#GLOBAL DEFAULTS::::::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------
+
+nuke.knobDefault('shutteroffset', "centered")
+nuke.knobDefault('Tracker4.shutteroffset', "centered")
+nuke.knobDefault('Tracker4.label', "Motion: [value transform]\nRef Frame: [value reference_frame]")
+
+
+
+nuke.addOnUserCreate(lambda:nuke.thisNode()
+['reference_frame'].setValue(nuke.frame()), nodeClass='Tracker4')
+
+nuke.addOnUserCreate(lambda:nuke.thisNode()
+['first_frame'].setValue(nuke.frame()), nodeClass='FrameHold')
+
+#---------------------------------------------------------
+#COSTUME MENU:::::::::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------
+
+utilitiesMenu = nuke.menu('Nuke').addMenu('Utilities')
+
+utilitiesMenu.addCommand('Autocrop', 'nukescripts.autocrop()')
+
+#---------------------------------------------------------
+#TOOLS MENU:::::::::::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------
+
+
+myGizmosMenu = nuke.menu('Nodes').addMenu('myGizmosMenu', icon="myGizmos_icon.png")
+myGizmosMenu.addCommand('bm_CameraShake', "nuke.createNode('bm_CameraShake')", icon="bm_CameraShake_icon.png")
+myGizmosMenu.addCommand('bm_CurveRemapper', "nuke.createNode('bm_CurveRemapper')", icon="bm_CurveRemapper_icon.png")
+myGizmosMenu.addCommand('bm_EdgeMatte', "nuke.createNode('bm_EdgeMatte')", icon="bm_EdgeMatte_icon.png")
+myGizmosMenu.addCommand('bm_Lightwrap', "nuke.createNode('bm_Lightwrap')", icon="bm_Lightwrap_icon.png")
+myGizmosMenu.addCommand('bm_MatteCheck', "nuke.createNode('bm_MatteCheck')", icon="bm_MatteCheck_icon.png")
+myGizmosMenu.addCommand('bm_NoiseGen', "nuke.createNode('bm_NoiseGen')", icon="bm_NoiseGen.png")
+myGizmosMenu.addCommand('bm_OpticalGlow', "nuke.createNode('bm_OpticalGlow')", icon="bm_OpticalGlow_icon.png")
+
+#---------------------------------------------------------
+#KeyBoard ShortCuts:::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------
+
+nuke.menu('Nodes').addCommand("Transform/Tracker", "nuke.createNode('Tracker4')", "ctrl+t", icon="Tracker.png", shortcutContext=2)
 
