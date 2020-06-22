e1ff42244cc01d79cda62d84823083539c156275
franciscopanchus <64598246+franciscopanchus@users.noreply.github.com>
2020-06-22 18:40:15 -0300

    Add files via upload

---
+++ python/testeo.py
@@ -0,0 +1,20 @@
+#---------------------------------------------------------	
+#filepathLister.py
+#Version 1.0.0
+#Last Update 06/05/20
+#---------------------------------------------------------
+
+import nuke
+import os
+
+def testeofuncion():
+	lista = []
+
+	for i in lista:
+		lista.append('1','2','3','4')
+		print lista
+
+	#Txt = lista	
+	#creation = open('C:/Users/franc/.nuke/Outtxt/test.txt', 'w')
+	#creation.write( Txt)
+
