3950ad8f5beb958d73247e8dfa6930474133928e
franciscopanchus <64598246+franciscopanchus@users.noreply.github.com>
2020-08-03 16:45:36 -0300

    Add files via upload

--- init.py
+++ init.py
@@ -1,10 +1,20 @@
 #---------------------------------------------------------	
 #init.py
 #Version 1.0.0
-#Last Update 30/04/20
+#Last Update 24/06/20
+#Francisco Biancardi
 #---------------------------------------------------------
 
 nuke.pluginAddPath('./gizmos')
 nuke.pluginAddPath('./python')
 nuke.pluginAddPath('./python/shuffleShortCut')
 nuke.pluginAddPath('./icons')
+nuke.pluginAddPath('./custumize_nodes/camera')
+nuke.pluginAddPath("renderFinished")
+nuke.pluginAddPath("revealInFinder")
+nuke.pluginAddPath("ToolEngine")
+nuke.pluginAddPath("./python/cryptomatte")
+nuke.pluginAddPath("./ReplaceFileName")
+
+#import cryptomatte_utilities
+#cryptomatte_utilities.setup_cryptomatte()
